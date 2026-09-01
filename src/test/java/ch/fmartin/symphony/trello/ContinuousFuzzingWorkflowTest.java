605eabf447dd4b515e775d22bdfc4b8091f94ebb
François Martin <f.martin@fastmail.com>
2026-09-01 08:41:51 +0200

    test(ci): make setup-java order guard version-independent

--- src/test/java/ch/fmartin/symphony/trello/ContinuousFuzzingWorkflowTest.java
+++ src/test/java/ch/fmartin/symphony/trello/ContinuousFuzzingWorkflowTest.java
@@ -267,7 +267,7 @@ final class ContinuousFuzzingWorkflowTest {
         // then
         assertThat(coverageJob)
                 .containsSubsequence(
-                        "uses: actions/setup-java@dd06d9cba3e5552c54d9f8ea23572deb30010f7c",
+                        "uses: actions/setup-java@",
                         "distribution: temurin",
                         "java-version: \"25\"",
                         "scripts/run-clusterfuzzlite-coverage");
